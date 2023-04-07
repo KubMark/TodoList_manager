987d48a6c1c8f4448b7b2ecf8676345d0a3a9316
macbook <mypur86@yahoo.com>
2023-04-08 00:14:34 +0300

    L33done

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
+
+from core.models import User
+
 
 # Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name')
+    search_fields = ('emailfirst_name', 'last_name', 'username')
+    list_filter = ('is_staff', 'is_active', 'is_superuser')
+    readonly_fields = ('last_login', 'date_joined')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permission', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')})
+    )
+
+
+admin.site.unregister(Group)
