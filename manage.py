a2dcdb2042c6767874a9db5ea41e70c5030c6233
macbook <mypur86@yahoo.com>
2023-04-11 11:18:06 +0300

    static_fix

--- manage.py
+++ manage.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
@@ -18,5 +16,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
