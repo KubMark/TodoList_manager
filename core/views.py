186c80da0f988b18a4017ed48be1843bfbd4072e
macbook <mypur86@yahoo.com>
2023-04-16 23:46:02 +0300

    drf/urls_added

--- core/views.py
+++ core/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from rest_framework import status
+from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
 
-# Create your views here.
+from core.models import User
+from core.serializers import CreateUserSerializer, LoginSerializer
+
+
+class CreateAccountView(CreateAPIView):
+    """ Ручка регистрации нового пользователя """
+    serializer_class = CreateUserSerializer
+    queryset = User.objects.all()
+
+
+class LoginView(CreateAPIView):
+    serializer_class = LoginSerializer
+
+    def create(self, request, *args, **kwargs) -> Response:
+        serializer: LoginSerializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_200_OK)
