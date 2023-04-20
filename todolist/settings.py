533f05d426d4a7ebcb0c9bbbd2cb94adf33148d8
macbook <mypur86@yahoo.com>
2023-04-20 11:07:44 +0300

    VK_OAUTH2

--- todolist/settings.py
+++ todolist/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "core",
+    "social_django",
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,16 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = 'core.User'
+
+SOCIAL_AUTH_JSONFIELD_ENABLED = True
+SOCIAL_AUTH_VK_OAUTH2_KEY = os.environ.get('VK_OAUTH2_KEY')
+SOCIAL_AUTH_VK_OAUTH2_SECRET = os.environ.get('VK_OAUTH2_SECRET')
+SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/'
+SOCIAL_AUTH_NEW_USER_REDIRECT_URL = '/logged-in/'
+SOCIAL_AUTH_USER_MODEL = 'core.User'
+SOCIAL_AUTH_VK_OAUTH2_SCOPE = ['email']
+AUTHENTICATION_BACKENDS = (
+    'social_core.backends.vk.VKOAuth2',
+    'django.contrib.auth.backends.ModelBackend',
+)
+SOCIAL_AUTH_LOGIN_ERROR_URL = '/login-error/'
--- todolist/urls.py
+++ todolist/urls.py
@@ -5,6 +5,8 @@ from django.conf import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("core/", include('core.urls')),
+    path("oauth/", include("social_django.urls", namespace="social"))
+
 ]
 if settings.DEBUG:
     urlpatterns += [
