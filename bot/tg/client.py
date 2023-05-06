3bd40c65678536f28b1f2a3f1cf0f23acd5fcc82
macbook <mypur86@yahoo.com>
2023-05-06 23:55:46 +0300

    telegram_bot

---
+++ bot/tg/client.py
@@ -0,0 +1,34 @@
+import logging
+import requests
+from pydantic import ValidationError
+from bot.tg.dc import GetUpdatesResponse, SendMessageResponse
+
+logger = logging.getLogger(__name__)
+
+
+class TgClient:
+    def __init__(self, token: str):
+        self.token = token
+
+    def get_url(self, method: str):
+        """Returns url to TG bot in str format with requested method"""
+        return f"https://api.telegram.org/bot{self.token}/{method}"
+
+    def get_updates(self, offset: int = 0, timeout: int = 60) -> GetUpdatesResponse:
+        """Requests TG bot using getUpdates"""
+        response = requests.get(self.get_url('getUpdates'), params={'timeout': timeout, 'offset': offset})
+        data = response.json()
+        try:
+            return GetUpdatesResponse(**data)
+        except ValidationError:
+            logger.error(f'Пришли не верные данные: {data}')
+
+
+    def send_message(self, chat_id: int, text: str) -> SendMessageResponse:
+        """Requests TG bot using sendMessage"""
+        response = requests.get(self.get_url('SendMessage'), params={'chat_id': chat_id, 'text': text}).json()
+        data = response.json()
+        try:
+            return SendMessageResponse(**data)
+        except ValidationError:
+            logger.error(f'Пришли не верные данные: {data}')
